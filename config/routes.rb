a3395379f52b0cb591e2784846ad7c7fa0d4fa12
Amrinder Singh <asingh@linfield.edu>
2016-05-17 20:48:31 -0700

    finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   get 'home/front'
 
   get 'static_pages/home'
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :articles
   root 'welcome#index'
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
